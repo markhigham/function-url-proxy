343d64de46ea8d8c33f7f87cbddc8a728e589d50
Mark Higham <mark.higham@gmail.com>
2022-05-26 23:25:36 +0100

    initial files

---
+++ index.js
@@ -0,0 +1,89 @@
+const argv = require("minimist")(process.argv.slice(2));
+const path = require("path");
+const showHelp = require("./usage");
+const express = require("express");
+const debug = require("debug")("function-url-proxy");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+
+let port = 3000;
+let host = "0.0.0.0";
+
+if (argv.h) {
+  showHelp();
+  process.exit(0);
+}
+
+if (argv._.length < 1) {
+  // We expect at least test_file
+  showHelp();
+  process.exit(-1);
+}
+
+if (argv.p) {
+  port = argv.p;
+}
+
+const moduleFilename = argv._[0];
+const filename = path.resolve(moduleFilename);
+debug(filename);
+
+let handlerName = "handler";
+if (argv.f) {
+  handlerName = argv.f;
+  debug(`handlerName: ${handlerName}`);
+}
+
+let handler;
+
+try {
+  const module = require(filename);
+  debug("handlerName", handlerName);
+  handler = module[handlerName];
+
+  if (!handler) {
+    console.error(
+      `Error: Couldn't find handler function '${handlerName}' in ${filename}`
+    );
+    process.exit(-1);
+  }
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
+
+if (handler.constructor.name !== "AsyncFunction") {
+  console.error(`Expected '${handlerName}' to be an AsyncFunction`.red);
+  process.exit(1);
+}
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all("*", async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  debug(`${req.method} ${fullUrl}`);
+  const u = new URL(fullUrl);
+
+  const packaged = {
+    queryStringParameters: req.query,
+    headers: req.headers,
+    rawQueryString: u.search,
+    rawPath: u.pathname,
+    requestContext: {
+      http: {
+        method: req.method,
+        path: u.pathname,
+      },
+      requestId: uuid.v4(),
+    },
+  };
+  debug(`calling ${moduleFilename.replace(".js", "")}.${handlerName}`);
+  const result = await handler(packaged);
+  res.send(result);
+});
+
+app.listen(port, host, () => {
+  console.log(`started on http://${host}:${port}`);
+});
---
+++ usage.js
@@ -0,0 +1,17 @@
+function showHelp() {
+  const pkg = require("./package.json");
+  const version = pkg.version;
+  console.log(`function-url-proxy [-f handler] test_file
+  version: ${version}
+  
+  test_file.js (required)
+      Path to a file containing the lambda handler
+  
+  -f (optional) Name of the handler function. Defaults to handler
+  
+  -p (optional) Port number. Defaults to 3000
+  
+      `);
+}
+
+module.exports = showHelp;
